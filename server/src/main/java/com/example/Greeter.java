91900f356a142cc0c55407b425601843d628620d
rutaborkar <33936960+rutaborkar@users.noreply.github.com>
2020-06-10 01:43:19 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,7 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
+    System.out.println("Hello nerds");
     return String.format("Hello, %s!", someone);
   }
 }
